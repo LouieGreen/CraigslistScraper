28888750f3d37b37a226cf475118204c0d960b31
Louie Green <LouieGreen@users.noreply.github.com>
2019-12-29 12:29:03 -0500

    Update Driver.java

--- src/net/louietech/craigslist/Driver.java
+++ src/net/louietech/craigslist/Driver.java
@@ -3,6 +3,7 @@ package net.louietech.craigslist;
 public class Driver {
 	public static void main(String[] args) {
 		FetchAndParse fetchAndParse = new FetchAndParse();
+		fetchAndParse.SetSendTextMessages(true);
 		fetchAndParse.Start();
 	}
 }
